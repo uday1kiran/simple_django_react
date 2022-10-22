8bee7f9bacc8e4e13dd8e3894133b2c8b47f5842
Your Name <you@example.com>
2022-10-22 14:24:18 +0530

    added get and post

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import hello, hello1
+from .views import hello1, hello2
 
 urlpatterns = [
-    path('hello', hello),
     path('hello1', hello1),
+    path('hello2', hello2),
 ]
